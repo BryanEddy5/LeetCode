de13618425ff5f293fa15cea0b01d34eed8ce249
Bryan Eddy <v-bryan.eddy@humanaedge.com>
2021-12-28 11:45:52 -0800

    init

---
+++ LeetCode/LicenseKeyInformation.cs
@@ -0,0 +1,35 @@
+using System.Linq;
+using System.Text;
+using Xunit;
+
+namespace LeetCode
+{
+    public class LicenseKeyInformation
+    {
+        public string LicenseKeyFormatting(string s, int k)
+        {
+            var S = s.Replace("-", "").ToUpper();
+            var sb = new StringBuilder();
+            for (int i = 0; i < S.Length; i++)
+            {
+                if (i != 0 && (S.Length - i) % k == 0)
+                {
+                    sb.Append('-');
+                }
+                sb.Append(S[i]);
+            }
+            return sb.ToString();
+
+        }
+
+        [Theory]
+        [InlineData("2-5g-3-J", 2, "2-5G-3J")]
+        public void Test(string input, int k, string expected)
+        {
+            var actual = LicenseKeyFormatting(input, 2);
+            
+            Assert.Equal(expected, actual);
+
+        }
+    }
+}
---
+++ LeetCode/SinglyListNode.cs
@@ -0,0 +1,14 @@
+namespace LeetCode
+{
+    public class SinglyListNode
+    {
+        int _val;
+        
+        SinglyListNode next;
+
+        SinglyListNode(int x)
+        {
+            _val = x;
+        }
+    }
+}
